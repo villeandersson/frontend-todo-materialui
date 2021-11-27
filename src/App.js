d4ac64a1569d7d96112422d0ef0e5a0a6aaacaf5
Ville Andersson <winstoni@windowslive.com>
2021-11-27 20:04:03 +0200

    todo-mat

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React, { useState, useRef } from "react";
 import "./App.css";
 import { AgGridReact } from "ag-grid-react";
+import { TextField, Button, Box } from "@material-ui/core/";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import DesktopDatePicker from "@mui/lab/DesktopDatePicker";
+import { getDate, getMonth, getYear } from "date-fns";
 
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-material.css";
 
+function Item(props) {
+  const { sx, ...other } = props;
+  return (
+    <Box
+      sx={{
+        p: 1,
+        m: 1,
+        ...sx,
+      }}
+      {...other}
+    />
+  );
+}
+
 function App() {
   const [todo, setTodo] = useState({ kuvaus: "", pvm: "", tarkeys: "" });
   const [todos, setTodos] = useState([]);
+  const [value, setValue] = useState(new Date());
 
   const gridRef = useRef();
 
@@ -44,8 +64,22 @@ function App() {
     setTodo({ ...todo, [event.target.name]: event.target.value });
   };
 
+  const handleChange = (newValue) => {
+    setValue(newValue);
+    setTodo({
+      ...todo,
+      pvm:
+        getDate(newValue) +
+        "-" +
+        (getMonth(newValue) + 1) +
+        "-" +
+        getYear(newValue),
+    });
+  };
+
   const addTodo = (event) => {
     event.preventDefault();
+    console.log(todo);
     setTodos([...todos, todo]);
   };
 
@@ -66,31 +100,65 @@ function App() {
     <div className="App">
       <h1 className="App-header">TODOLIST</h1>
       <div className="App-input">
-        Description:
-        <input
-          style={{ marginRight: 10 }}
-          type="text"
-          value={todo.kuvaus}
-          name="kuvaus"
-          onChange={inputChanged}
-        />
-        Date:
-        <input
-          style={{ marginRight: 10 }}
-          type="date"
-          value={todo.pvm}
-          name="pvm"
-          onChange={inputChanged}
-        />
-        Priority:
-        <input
-          type="text"
-          value={todo.tarkeys}
-          name="tarkeys"
-          onChange={inputChanged}
-        />
-        <button onClick={addTodo}>Add</button>
-        <button onClick={deleteTodo}>Delete</button>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "flex-end",
+            p: 1,
+            m: 1,
+          }}
+        >
+          <Item>
+            <TextField
+              type="text"
+              placeholder="Task"
+              value={todo.kuvaus}
+              name="kuvaus"
+              onChange={inputChanged}
+            />
+          </Item>
+          <Item>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <DesktopDatePicker
+                label="Date desktop"
+                inputFormat="dd/MM/yyyy"
+                value={value}
+                onChange={handleChange}
+                renderInput={(params) => <TextField {...params} />}
+              />
+            </LocalizationProvider>
+          </Item>
+          <Item>
+            <TextField
+              type="text"
+              placeholder="Priority"
+              value={todo.tarkeys}
+              name="tarkeys"
+              onChange={inputChanged}
+            />
+          </Item>
+          <Item>
+            <Button
+              size="small"
+              variant="contained"
+              color="primary"
+              onClick={addTodo}
+            >
+              Add
+            </Button>
+          </Item>
+          <Item>
+            <Button
+              size="small"
+              variant="contained"
+              color="primary"
+              onClick={deleteTodo}
+            >
+              Delete
+            </Button>
+          </Item>
+        </Box>
       </div>
       <div
         className="ag-theme-material"
